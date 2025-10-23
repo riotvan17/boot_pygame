7be514ed426f52012862bdb2e13b8d0a65bec57f
Ed Jelizarov <eduards.jelizarovs@gmail.com>
2025-10-23 09:52:27 +0200

    finished game

---
+++ circleshape.py
@@ -0,0 +1,27 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+	def __init__(self, x, y, radius):
+		if hasattr(self, "containers"):
+			super().__init__(self.containers)
+		else:
+			super().__init__()
+
+		self.position = pygame.Vector2(x, y)
+		self.velocity = pygame.Vector2(0, 0)
+		self.radius = radius
+
+	def draw(self, screen):
+		# sub-classes must override
+		pass
+
+	def update(self, dt):
+		# sub-classes must override
+		pass
+
+	def collision(self, circle_shape):
+		distance = self.position.distance_to(circle_shape.position)
+		if distance <= self.radius + circle_shape.radius:
+			return True
+		return False
--- main.py
+++ main.py
@@ -1,15 +1,49 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	Asteroid.containers = (asteroids, updatable, drawable)
+	Player.containers = (updatable, drawable)
+	AsteroidField.containers = (updatable)
+	Shot.containers = (shots, updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_obj = AsteroidField()
+
+
 	while 1:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill(color="black")
+		screen.fill("black")
+		updatable.update(dt)
+		for a in asteroids:
+			if(a.collision(player)):
+				print("Game over!")
+				return
+		for a2 in asteroids:
+			for s in shots:
+				if(a2.collision(s)):
+					s.kill()
+					a2.split()
+
+		for item in drawable:
+			item.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+
+
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
